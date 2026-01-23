7e62d4f6e29c093acae5fe86e4f137ede8207b70
yasinarafatajad <yasinarafatazad082@gmail.com>
2026-01-23 21:19:11 +0600

    chore(ci): add CI/CD pipeline configuration

--- SERVER/src/routes/products.js
+++ SERVER/src/routes/products.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { AddProduct, DeleteProduct, GetAllProducts, GetProduct } from "../controllers/product.js";
+import { AddProduct, DeleteProduct, GetAllProducts, GetProduct, UpdateProduct } from "../controllers/product.js";
 
 const router = express.Router();
 
-router.get('/AllProducts' , GetAllProducts)
-router.get('/Product/:id' , GetProduct)
 router.post('/AddProduct' , AddProduct)
+router.post('/UpdateProduct' , UpdateProduct)
+router.get('/Product/:id' , GetProduct)
+router.get('/AllProducts' , GetAllProducts)
 router.delete('/DeleteProduct/:id' , DeleteProduct)
 
 export default router;

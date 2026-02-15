a85307799f1ab7bd8f951c2e012bff26260154c4
yasinarafatajad <yasinarafatazad082@gmail.com>
2026-02-15 21:48:33 +0600

    [backend]: customer register,update add order api

--- SERVER/src/controllers/customer.js
+++ SERVER/src/controllers/customer.js
@@ -39,3 +39,33 @@ export const addCustomer = async (req, res) => {
         console.log(err);
     }
 }
+export const deleteCustomer = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await customerModel.findByIdAndDelete(id);
+        if (result) {
+            res.status(200).json('one customer deleted')
+        } else {
+            res.status(404).json('customer not found')
+        }
+    } catch (err) {
+        console.log(err);
+
+    }
+}
+export const updateCustomer = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await customerModel.findByIdAndUpdate(
+            id,
+            { $set: req.body }
+        )
+        if (result) {
+            res.status(200).json('customer data updated', result)
+        } else {
+            res.status(500).json('customer not found')
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
